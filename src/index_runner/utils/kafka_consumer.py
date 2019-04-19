a6004c08416dd7c5dc165ad70b2dbb0faffe165c
slebras <slebras@Sebastians-MBP.dhcp.lbnl.us>
2019-04-19 11:27:50 -0700

    adding timeout for kafka consumer and new test for timeout

--- src/index_runner/utils/kafka_consumer.py
+++ src/index_runner/utils/kafka_consumer.py
@@ -3,12 +3,19 @@ Convenience wrapper / generator function arounda kafka consumer for a given
 topic/client group.
 """
 import json
+import time
 from confluent_kafka import Consumer, KafkaError
 
 from .config import get_config
 
 
-def kafka_consumer(topics):
+def check_timeout(start_time, timeout):
+    elapsed = time.time() - start_time
+    if elapsed > timeout:
+        raise TimeoutError(f"Error: Consumer waited past timeout of {timeout} seconds")
+
+
+def kafka_consumer(topics, timeout=60):
     """
     Generator of kafka messages for a given set of topics.
     """
@@ -20,7 +27,9 @@ def kafka_consumer(topics):
     })
     consumer.subscribe(topics)
     print(f"Listening to {topics} in group {config['kafka_clientgroup']}")  # noqa
+    start_time = time.time()
     while True:
+        check_timeout(start_time, timeout)
         msg = consumer.poll(0.5)
         if msg is None:
             continue
@@ -37,5 +46,7 @@ def kafka_consumer(topics):
             # JSON parsing error
             print('JSON message error:', err)
             continue
+        # reset start time everytime we yield something
+        start_time = time.time()
         yield data
     consumer.close()
