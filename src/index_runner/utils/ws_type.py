ca4b7f93e861d34328939476f06cb8161e557d8c
Jay R Bolton <jayrbolton@gmail.com>
2019-05-08 12:55:59 -0700

    Fix ws_type.get_name

--- src/index_runner/utils/ws_type.py
+++ src/index_runner/utils/ws_type.py
@@ -6,6 +6,6 @@ def get_pieces(type_str):
      - Given "KBaseNarrative.Narrative-4.0"
      - Returns ("KBaseNarrative", "Narrative", "4.0")
     """
-    (type_module_name, type_version) = type_str.split('-')
-    (type_module, type_name) = type_module_name.split('.')
-    return (type_module_name, type_name, type_version)
+    (full_name, type_version) = type_str.split('-')
+    (type_module, type_name) = full_name.split('.')
+    return (type_module, type_name, type_version)
