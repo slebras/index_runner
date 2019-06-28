d1bff8dd04196962eb045b3de455faeba6c33776
Jay R Bolton <jayrbolton@gmail.com>
2019-06-28 16:02:10 -0700

    Get rid of logs;

--- src/index_runner/index_runner.py
+++ src/index_runner/index_runner.py
@@ -93,9 +93,7 @@ class IndexRunner:
                 self._log_err_to_es(msg)
                 continue
             # Push to the elasticsearch write queue
-            print('index_runner sending...')
             self.sock.send_json(result)
-            print('index_runner sent.')
 
     def _run_obj_deleter(self, msg):
         """
@@ -108,9 +106,7 @@ class IndexRunner:
             # Object is not deleted
             print(f'object {objid} in workspace {wsid} not deleted')
             return
-        print('index_runner sending...')
         self.sock.send_json({'_action': 'delete', 'object_id': f"{wsid}:{objid}"})
-        print('index_runner sent.')
 
     def _run_workspace_deleter(self, msg):
         """
@@ -122,9 +118,7 @@ class IndexRunner:
         if not check_workspace_deleted(wsid):
             print(f'Workspace {wsid} not deleted')
             return
-        print('index_runner sending...')
         self.sock.send_json({'_action': 'delete', 'workspace_id': str(wsid)})
-        print('index_runner sent.')
 
     def _clone_workspace(self, msg):
         """
@@ -148,13 +142,11 @@ class IndexRunner:
         workspace_id = msg['wsid']
         is_public = is_workspace_public(workspace_id)
         # Push the event to the elasticsearch writer queue
-        print('index_runner sending...')
         self.sock.send_json({
             '_action': 'set_global_perm',
             'workspace_id': workspace_id,
             'is_public': is_public
         })
-        print('index_runner sent.')
 
     def _index_nonexistent(self, msg):
         """
@@ -172,11 +164,9 @@ class IndexRunner:
         # The key is a hash of the message data body
         # The index document is the error string plus the message data itself
         _id = hashlib.blake2b(json.dumps(msg).encode('utf-8')).hexdigest()
-        print('index_runner sending...')
         self.sock.send_json({
             '_action': 'index',
             'index': _CONFIG['error_index_name'],
             'id': _id,
             'doc': {'error': str(err), **msg}
         })
-        print('index_runner sent.')
