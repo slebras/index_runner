8f6c93b2360802af5573edc808a4ce3d2bde72ae
Jay R Bolton <jayrbolton@gmail.com>
2020-01-02 13:57:17 -0800

    1.2.2

--- scripts/local-deploy.sh
+++ scripts/local-deploy.sh
@@ -2,6 +2,6 @@
 set -e
 # show the commands we execute
 set -o xtrace
-export IMAGE_NAME="kbase/index_runner2:1.2.1"
+export IMAGE_NAME="kbase/index_runner2:1.2.2"
 sh hooks/build
 docker push $IMAGE_NAME
