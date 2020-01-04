c1876b62d268cbe6b1f9695bc92be357fd6e2bb5
Jay R Bolton <jayrbolton@gmail.com>
2020-01-03 18:17:17 -0800

    Use assert_contains and add test for deletion

--- src/test/test_integration.py
+++ src/test/test_integration.py
@@ -2,24 +2,45 @@ import unittest
 import json
 import time
 import requests
-from multiprocessing import Process
+import logging
 from confluent_kafka import Producer
 
 from src.utils.config import config
-from src.index_runner.main import main
 from src.utils.re_client import get_doc, get_edge
 
-_TEST_EVENT = {
-   "wsid": 41347,
-   "ver": 16,
-   "perm": None,
-   "evtype": "NEW_VERSION",
-   "objid": 5,
-   "time": 1554408508419,
-   "objtype": "KBaseNarrative.Narrative-4.0",
-   "permusers": [],
-   "user": "username"
-}  # type: dict
+logger = logging.getLogger('IR')
+
+_TEST_EVENTS = {
+    # This object will not be found in the listObjects method
+    "new_object_version": {
+       "wsid": 41347,
+       "ver": 16,
+       "perm": None,
+       "evtype": "NEW_VERSION",
+       "objid": 5,
+       "time": 1554408508419,
+       "objtype": "KBaseNarrative.Narrative-4.0",
+       "permusers": [],
+       "user": "username"
+    },
+    "deleted_object": {
+        "wsid": 41347,
+        "ver": None,
+        "perm": None,
+        "evtype": "OBJECT_DELETE_STATE_CHANGE",
+        "objid": 5,
+        "time": 1554408311320,
+        "objtype": None,
+        "permusers": [],
+        "user": "username"
+    },
+    "set_global_permission": {
+        "wsid": 41347,
+        "evtype": "SET_GLOBAL_PERMISSION",
+        "time": 1559165387436,
+        "user": "username"
+    }
+}
 
 
 class TestIntegration(unittest.TestCase):
@@ -28,24 +49,17 @@ class TestIntegration(unittest.TestCase):
     We can produce a message to a kafka topic, it gets picked up by
     index_runner, it gets pushed to es_writer, and then it gets saved to ES.
     """
-
-    @classmethod
-    def setUpClass(cls):
-        cls.proc = Process(target=main)
-        cls.proc.start()
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.proc.kill()
+    maxDiff = None
 
     def test_basic(self):
         """
-        Test the full workflow from kafka to ES.
+        Test the full workflow from kafka to ES and RE.
         This just validates that things are connected.
         """
         # Produce an event on Kafka
-        _produce(_TEST_EVENT)
-        _id = f"WS::{_TEST_EVENT['wsid']}:{_TEST_EVENT['objid']}"
+        ev = _TEST_EVENTS['new_object_version']
+        _produce(ev)
+        _id = f"WS::{ev['wsid']}:{ev['objid']}"  # type: ignore
         # Fetch the doc from Elasticsearch
         doc = _get_es_doc_blocking(_id)
         self.assertEqual(doc['_id'], _id)
@@ -144,6 +158,10 @@ class TestIntegration(unittest.TestCase):
             'ws_object_version/2:2:2',  # to
         )
         self.assertTrue(prov_edge2)
+        del_ev = _TEST_EVENTS['deleted_object']
+        _produce(del_ev)
+        del_id = f"WS::{del_ev['wsid']}:{del_ev['objid']}"  # type: ignore
+        _check_deleted_es_doc_blocking(del_id)
 
     def test_import_nonexistent_existing(self):
         """Test an IMPORT_NONEXISTENT event."""
@@ -170,15 +188,32 @@ class TestIntegration(unittest.TestCase):
 
 def _delivery_report(err, msg):
     if err is not None:
-        print('Message delivery failed:', err)
+        logger.error(f'Message delivery failed: {err}')
     else:
-        print('Message delivered to', msg.topic())
+        logger.info(f'Message delivered to: {msg.topic()}')
 
 
 def _produce(data, topic=config()['topics']['workspace_events']):
     producer = Producer({'bootstrap.servers': config()['kafka_server']})
     producer.produce(topic, json.dumps(data), callback=_delivery_report)
     producer.poll(60)
+
+
+def _check_deleted_es_doc_blocking(_id, timeout=180):
+    """
+    Helper for testing deletion. Inverse function of _get_es_doc_blocking
+    Try to fetch a doc on ES, returning successfully once the doc does not exist.
+    """
+    start_time = int(time.time())
+    while True:
+        result = _get_es_doc(_id)
+        logger.info('result is', result)
+        if not result:
+            return None
+        if (int(time.time()) - start_time) > timeout:
+            raise RuntimeError(f"Document {_id} was not deleted in {timeout}s.")
+        print(f'Waiting for document {_id} to be deleted')
+        time.sleep(5)
 
 
 def _get_es_doc_blocking(_id, timeout=180):
@@ -194,7 +229,6 @@ def _get_es_doc_blocking(_id, timeout=180):
 
 
 def _get_es_aliases_blocking(timeout=180):
-    """"""
     start_time = int(time.time())
     while True:
         result = _get_es_aliases()
@@ -208,7 +242,7 @@ def _get_es_aliases_blocking(timeout=180):
 def _get_es_doc(_id):
     """Fetch a document from elastic based on ID."""
     prefix = config()['elasticsearch_index_prefix']
-    print(f'Fetching {_id} from index {prefix}.*')
+    logger.info(f'Fetching {_id} from index {prefix}.*')
     url = f"{config()['elasticsearch_url']}/{prefix}.*/_search?size=1"
     resp = requests.post(
         url,
@@ -273,123 +307,3 @@ def _wait_for_re_doc(coll, key, timeout=180):
                 raise RuntimeError('Timed out trying to fetch', key)
             time.sleep(1)
     return results['results'][0]
-
-# _HEADERS = {"Content-Type": "application/json"}
-# _TEST_EVENTS = {
-#     # This object will not be found in the listObjects method
-#     'new_object_version': {
-#         "wsid": 41347,
-#         "ver": 1,
-#         "perm": None,
-#         "evtype": "NEW_VERSION",
-#         "objid": 6,
-#         "time": 1554404277444,
-#         "objtype": "KBaseGenomes.Genome-14.2",
-#         "permusers": [],
-#         "user": "username"
-#     },
-#     'deleted_object': {
-#         "wsid": 41347,
-#         "ver": None,
-#         "perm": None,
-#         "evtype": "OBJECT_DELETE_STATE_CHANGE",
-#         "objid": 5,
-#         "time": 1554408311320,
-#         "objtype": None,
-#         "permusers": [],
-#         "user": "username"
-#     },
-#     'set_global_permission': {
-#         "wsid": 41347,
-#         "evtype": "SET_GLOBAL_PERMISSION",
-#         "time": 1559165387436,
-#         "user": "username"
-#     }
-# }
-#
-#
-# class TestIntegration(unittest.TestCase):
-#
-#     maxDiff = None
-#
-#     @classmethod
-#     def setUpClass(cls):
-#         # Start the index runner
-#         self.indexer = ThreadGroup(IndexRunner, (frontend_url,), count=1)
-#         # Start the es_writer
-#         self.writer = ThreadGroup(ESWriter, (backend_url,), count=1)
-#
-#     def test_object_save_and_delete(self):
-#         """
-#         Test a NEW_VERSION event plus an OBJECT_DELETE_STATE_CHANGE.
-#         """
-#         print(f"Producing NEW_VERSION event to {config()['topics']['workspace_events']}")
-#         _produce(_TEST_EVENTS['narrative_save_nonexistent'])
-#         # _id = 'WS::41347:5'
-#         # doc = _get_doc(_id)
-#         # self.assertEqual(doc['_id'], _id)
-#         print(f"Producing OBJECT_DELETE_STATE_CHANGE event to {config()['topics']['workspace_events']}")
-#         _produce(_TEST_EVENTS['deleted_object'])
-#         # time.sleep(30)
-#         # with self.assertRaises(RuntimeError):
-#         #     _get_doc(_id)
-#         print("Producing a SET_GLOBAL_PERMISSION event.")
-#         _produce(_TEST_EVENTS['set_global_permission'])
-#         # time.sleep(30)
-#
-#     @unittest.skip('x')
-#     def test_index_nonexistent(self):
-#         _produce({
-#             'evtype': 'INDEX_NONEXISTENT',
-#             'wsid': 41347,
-#             'objid': 5
-#         })
-#
-#     @unittest.skip('x')
-#     def test_es_error_logging(self):
-#         """
-#         Test an event that throws an error while trying to index. We check that
-#         an error gets logged to ES.
-#         """
-#         print('Producing an invalid NEW_VERSION event..')
-#         _produce({
-#             'evtype': 'NEW_VERSION',
-#             'wsid': 1,
-#             # Invalid message; missing data
-#         })
-#         # time.sleep(30)
-#         # _id = 'xyz'  # todo
-#         # doc = _get_doc(_id)
-#         # self.assertEqual(doc['_id'], _id)
-#
-#
-# def _consume_last(topic, key, timeout=120):
-#     """Consume the most recent message from the topic stream."""
-#     consumer = Consumer({
-#         'bootstrap.servers': config()['kafka_server'],
-#         'group.id': 'test_only',
-#         'auto.offset.reset': 'earliest'
-#     })
-#     consumer.subscribe([topic])
-#     start_time = time.time()
-#     while True:
-#         # get time elapsed in seconds
-#         elapsed = time.time() - start_time
-#         if elapsed > timeout:
-#             # close consumer before throwing error
-#             consumer.close()
-#             raise TimeoutError(f"Error: Consumer waited past timeout of {timeout} seconds")
-#         msg = consumer.poll(0.5)
-#         if msg is None:
-#             continue
-#         if msg.error():
-#             if msg.error().code() == KafkaError._PARTITION_EOF:
-#                 print('End of stream.')
-#             else:
-#                 print(f"Error: {msg.error()}")
-#             continue
-#         if msg.key() != key:
-#             continue
-#         # We got our message
-#         consumer.close()
-#         return json.loads(msg.value())
