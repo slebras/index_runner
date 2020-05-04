52b28282ba7f3d72c733dd9b6e5fbcbcb6925a1f
Jay R Bolton <jayrbolton@gmail.com>
2020-05-04 15:23:51 -0700

    1.5.5->1.5.6 and add CHANGELOG.md

--- scripts/local-deploy.sh
+++ scripts/local-deploy.sh
@@ -2,6 +2,6 @@
 set -e
 # show the commands we execute
 set -o xtrace
-export IMAGE_NAME="kbase/index_runner2:1.5.5"
+export IMAGE_NAME="kbase/index_runner2:1.5.6"
 sh hooks/build
 docker push $IMAGE_NAME
