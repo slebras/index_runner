e0583d9463f08c18df8bfaa06ca542ba6d5455e7
slebras <slebras@lbl.gov>
2020-07-22 09:42:08 -0700

    Adding comments to SampleSet indexer. Adding KBaseSets.SampleSet to accepted types for indexing.

--- src/index_runner/es_indexers/sample_set.py
+++ src/index_runner/es_indexers/sample_set.py
@@ -17,7 +17,7 @@ _SAMPLE_INDEX_NAME = 'sample_' + str(_SAMPLE_INDEX_VERSION)
 # _VER_SAMPLE_INDEX_NAME = 'sample_version_' + str(_SAMPLE_INDEX_VERSION)
 
 
-def get_sample(sample_info):
+def _get_sample(sample_info):
     """ Get sample from SampleService
     sample_info - dict containing 'id' and 'version' of a sample
     """
@@ -34,6 +34,8 @@ def get_sample(sample_info):
         "version": "1.1"
     }
     resp = requests.post(url=config()['sample_service_url'], headers=headers, data=json.dumps(payload))
+    if not resp.ok:
+        raise RuntimeError(f"Returned from sample service with status {resp.status_code} - {resp.text}")
     resp_json = resp.json()
     if resp_json.get('error'):
         raise RuntimeError(f"Error from SampleService - {resp_json['error']}")
@@ -41,7 +43,12 @@ def get_sample(sample_info):
     return sample
 
 
-def flatten_meta(meta, prefix=None):
+def _flatten_meta(meta, prefix=None):
+    """ Flattens metadata fields in a Sample object. Fields are concatenated into a 
+        single string field to save into an Elasticsearch index
+    meta   - Sample Metadata to be flattened
+    prefix - (optional) prefix for the metadata values. default=None
+    """
     new_meta = {}
     for key in meta:
         if prefix:
@@ -56,12 +63,20 @@ def flatten_meta(meta, prefix=None):
     return new_meta
 
 
-def combine_meta(meta, flattened_meta, ite):
+def _combine_meta(meta, flattened_meta, idx):
+    """ Combine newly flattened metadata with existing metadata. This Function is designed to keep the indexing
+        of the different metadata fields consistent for each node within the sample node tree s.t. all the
+        fields in index (idx) 0 will be from item 0 in the node tree. Empty string ("") entries are Empty and
+        added simply so that the indexing of all fields line up.
+    meta           - existing metadata.
+    flattened_meta - newly flattened metadata.
+    idx            - current index of ndoe_tree.
+    """
     for key in flattened_meta:
         if key in meta:
-            meta[key] += ["" for _ in range(ite - len(meta[key]))] + [flattened_meta[key]]
+            meta[key] += ["" for _ in range(idx - len(meta[key]))] + [flattened_meta[key]]
         else:
-            meta[key] = ["" for _ in range(ite)] + [flattened_meta[key]]
+            meta[key] = ["" for _ in range(idx)] + [flattened_meta[key]]
     return meta
 
 
@@ -96,34 +111,34 @@ def index_sample_set(obj_data, ws_info, obj_data_v1):
 
     for samp in data["samples"]:
         # query the sample service for sample
-        sample = get_sample(samp)
+        sample = _get_sample(samp)
         sample_id = f"{_SAMPLE_NAMESPACE}::{sample['id']}:{sample['version']}"
         # not sure on how we need to handle more than 1 node.
         if len(sample['node_tree']) == 1:
-            meta_controlled = flatten_meta(
+            meta_controlled = _flatten_meta(
                 sample['node_tree'][0]['meta_controlled']
             )
-            meta_user = flatten_meta(
+            meta_user = _flatten_meta(
                 sample['node_tree'][0]['meta_user']
             )
         else:
             meta_controlled, meta_user = {}, {}
-            for ite, node in enumerate(sample['node_tree']):
-                meta_controlled = combine_meta(
+            for idx, node in enumerate(sample['node_tree']):
+                meta_controlled = _combine_meta(
                     meta_controlled,
-                    flatten_meta(
+                    _flatten_meta(
                         node['meta_controlled'],
                         prefix=node['id']
                     ),
-                    ite
+                    idx
                 )
-                meta_user = combine_meta(
+                meta_user = _combine_meta(
                     meta_user,
-                    flatten_meta(
+                    _flatten_meta(
                         node['meta_user'],
                         prefix=node['id']
                     ),
-                    ite
+                    idx
                 )
 
         sample_index = {
