abdf8e710ea1d44ae70554a34330cdd4e56d432a
Jay R Bolton <jayrbolton@gmail.com>
2020-10-23 15:17:10 -0700

    Convert integration test to pytest

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -1,5 +1,11 @@
-import unittest
+"""
+Integration test to confirm that the pieces of the system are successfully
+interconnected. We produce a message to a kafka topic and confirm something
+gets saved to ES and Arango.
 
+To test more detailed functionality of indexers/importers, write separate
+unit tests. Including them here will be too slow.
+"""
 import tests.helpers as helpers
 
 _TEST_EVENT = {
@@ -13,28 +19,17 @@ _TEST_EVENT = {
 }
 
 
-class TestIntegration(unittest.TestCase):
-    """
-    Integration test to confirm that the pieces of the system are successfully
-    interconnected. We produce a message to a kafka topic and confirm something
-    gets saved to ES and Arango.
-
-    To test more detailed functionality of indexers/importers, write separate
-    unit tests. Including them here will be too slow.
-    """
-    maxDiff = None
-
-    def test_integration(self):
-        # Produce the new object event on Kafka
-        helpers.produce(_TEST_EVENT)
-        wsid = _TEST_EVENT['wsid']  # type: ignore
-        objid = _TEST_EVENT['objid']  # type: ignore
-        es_id = f"WS::{wsid}:{objid}"  # type: ignore
-        re_key = f"{wsid}:{objid}"
-        es_doc = helpers.get_es_doc_blocking(es_id)
-        re_doc = helpers.wait_for_re_doc('ws_object', re_key)
-        self.assertEqual(es_doc['_id'], es_id)
-        self.assertEqual(re_doc['workspace_id'], wsid)
-        self.assertEqual(re_doc['object_id'], objid)
-        log_doc = helpers.get_es_doc_blocking(_TEST_EVENT['time'])
-        self.assertEqual(log_doc['_source'], _TEST_EVENT)
+def test_integration(self):
+    # Produce the new object event on Kafka
+    helpers.produce(_TEST_EVENT)
+    wsid = _TEST_EVENT['wsid']  # type: ignore
+    objid = _TEST_EVENT['objid']  # type: ignore
+    es_id = f"WS::{wsid}:{objid}"  # type: ignore
+    re_key = f"{wsid}:{objid}"
+    es_doc = helpers.get_es_doc_blocking(es_id)
+    re_doc = helpers.wait_for_re_doc('ws_object', re_key)
+    assert es_doc['_id'] == es_id
+    assert re_doc['workspace_id'] == wsid
+    assert re_doc['object_id'] == objid
+    log_doc = helpers.get_es_doc_blocking(_TEST_EVENT['time'])
+    assert log_doc['_source'] == _TEST_EVENT
